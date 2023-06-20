77de5698def1491913d7b6c9ce8190cd9c4664b3
numata <c0a2211625@edu.teu.ac.jp>
2023-06-20 14:49:02 +0900

    練習３

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,7 +4,13 @@ import pygame as pg
 
 
 WIDTH, HEIGHT = 1600, 900
+delta = {
+    pg.K_UP:(0,-5,),
+    pg.K_DOWN:(0,+5),
+    pg.K_LEFT:(-5,0),
+    pg.K_RIGHT:(+5,0),    
 
+}
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -12,6 +18,8 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 900,400
     clock = pg.time.Clock()
     bd_img=pg.Surface((20,20))
     bd_img.set_colorkey((0, 0, 0))
@@ -22,6 +30,8 @@ def main():
     bd_rct = bd_img.get_rect()
     bd_rct.center = x,y
     vx,vy = +5,+5
+    
+   
 
     tmr = 0
     while True:
@@ -30,16 +40,22 @@ def main():
                 return
         
        # bd_rct.center = 300,200
-        
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0,0]
+        for k, mv in delta.items():
+            if key_lst[k]:
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+        kk_rct.move_ip(sum_mv)
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        screen.blit(kk_img, kk_rct)
         bd_rct.move_ip(vx,vy)
     
         screen.blit(bd_img,bd_rct)
         pg.display.update()
         tmr += 1
-        clock.tick(10)
         clock.tick(50)
+
 
 
 if __name__ == "__main__":
